be2298420fd2d733f239ed76fda96d5fcf127304
Guilherme Oliveira Barbosa <41788461+TheGuix38@users.noreply.github.com>
2018-11-16 14:27:52 -0200

    Game Evil Genius
    Game developed for the Entrepreneur Fair, from Franco da Rocha ETEC.

---
+++ ProjetoEvilGenius/ProjetoEvilGenius/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace ProjetoEvilGenius
+{
+    partial class frmGenius
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmGenius));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.lblPontos = new System.Windows.Forms.Label();
+            this.btnIniciar = new System.Windows.Forms.Button();
+            this.btnSobre = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.DarkRed;
+            this.pictureBox1.Image = global::ProjetoEvilGenius.Properties.Resources.Evil_Genius;
+            this.pictureBox1.Location = new System.Drawing.Point(300, 431);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Tag = "R";
+            this.pictureBox1.Click += new System.EventHandler(this.Clique);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.DarkGreen;
+            this.pictureBox2.Image = global::ProjetoEvilGenius.Properties.Resources.Evil_Genius;
+            this.pictureBox2.Location = new System.Drawing.Point(406, 431);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Tag = "G";
+            this.pictureBox2.Click += new System.EventHandler(this.Clique);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.MidnightBlue;
+            this.pictureBox3.Image = global::ProjetoEvilGenius.Properties.Resources.Evil_Genius;
+            this.pictureBox3.Location = new System.Drawing.Point(300, 537);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Tag = "B";
+            this.pictureBox3.Click += new System.EventHandler(this.Clique);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.DarkGoldenrod;
+            this.pictureBox4.Image = global::ProjetoEvilGenius.Properties.Resources.Evil_Genius;
+            this.pictureBox4.Location = new System.Drawing.Point(406, 537);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Tag = "Y";
+            this.pictureBox4.Click += new System.EventHandler(this.Clique);
+            // 
+            // lblPontos
+            // 
+            this.lblPontos.AutoSize = true;
+            this.lblPontos.BackColor = System.Drawing.Color.Transparent;
+            this.lblPontos.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPontos.ForeColor = System.Drawing.Color.Lime;
+            this.lblPontos.Location = new System.Drawing.Point(271, 375);
+            this.lblPontos.Name = "lblPontos";
+            this.lblPontos.Size = new System.Drawing.Size(129, 25);
+            this.lblPontos.TabIndex = 4;
+            this.lblPontos.Text = "Pontuação:";
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.AutoSize = true;
+            this.btnIniciar.BackColor = System.Drawing.Color.Transparent;
+            this.btnIniciar.BackgroundImage = global::ProjetoEvilGenius.Properties.Resources.btn_iniciar;
+            this.btnIniciar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnIniciar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnIniciar.FlatAppearance.BorderSize = 0;
+            this.btnIniciar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btnIniciar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btnIniciar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnIniciar.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIniciar.ForeColor = System.Drawing.Color.Lime;
+            this.btnIniciar.Location = new System.Drawing.Point(8, 527);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(202, 140);
+            this.btnIniciar.TabIndex = 6;
+            this.btnIniciar.UseVisualStyleBackColor = false;
+            this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click);
+            // 
+            // btnSobre
+            // 
+            this.btnSobre.BackColor = System.Drawing.Color.Transparent;
+            this.btnSobre.BackgroundImage = global::ProjetoEvilGenius.Properties.Resources.btn_sobre;
+            this.btnSobre.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnSobre.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSobre.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btnSobre.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btnSobre.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSobre.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSobre.ForeColor = System.Drawing.Color.Transparent;
+            this.btnSobre.Location = new System.Drawing.Point(51, 633);
+            this.btnSobre.Name = "btnSobre";
+            this.btnSobre.Size = new System.Drawing.Size(202, 76);
+            this.btnSobre.TabIndex = 7;
+            this.btnSobre.UseVisualStyleBackColor = false;
+            this.btnSobre.Click += new System.EventHandler(this.btnSobre_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.BackColor = System.Drawing.Color.Transparent;
+            this.btnSair.BackgroundImage = global::ProjetoEvilGenius.Properties.Resources.btn_sair;
+            this.btnSair.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnSair.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSair.FlatAppearance.BorderSize = 0;
+            this.btnSair.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btnSair.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btnSair.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSair.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSair.ForeColor = System.Drawing.Color.Transparent;
+            this.btnSair.Location = new System.Drawing.Point(60, 715);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(277, 74);
+            this.btnSair.TabIndex = 8;
+            this.btnSair.UseVisualStyleBackColor = false;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // frmGenius
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = global::ProjetoEvilGenius.Properties.Resources.form_silhouete;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(800, 800);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnSobre);
+            this.Controls.Add(this.btnIniciar);
+            this.Controls.Add(this.lblPontos);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.DoubleBuffered = true;
+            this.ForeColor = System.Drawing.Color.Black;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmGenius";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Evil Genius";
+            this.TransparencyKey = System.Drawing.Color.White;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Label lblPontos;
+        private System.Windows.Forms.Button btnIniciar;
+        private System.Windows.Forms.Button btnSobre;
+        private System.Windows.Forms.Button btnSair;
+    }
+}
+
---
+++ ProjetoEvilGenius/ProjetoEvilGenius/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using System.Threading;
+
+namespace ProjetoEvilGenius
+{
+    public partial class frmGenius : Form
+    {
+        List<string> sequenciaCores = new List<string>();
+        List<string> sequenciaJogador = new List<string>();
+        int listaIndex, pontos;
+        const float brilho = 0.80f;
+        string atualCor;
+        string[] cores = { "R", "G", "B", "Y" };
+        bool podeJogar;
+        Random rdn = new Random();
+        SoundPlayer[] audios = { new SoundPlayer(Application.StartupPath + "\\audio02\\beep01.wav"), new SoundPlayer(Application.StartupPath + "\\audio02\\beep02.wav"), new SoundPlayer(Application.StartupPath + "\\audio02\\beep03.wav"), new SoundPlayer(Application.StartupPath + "\\audio02\\beep04.wav") };
+
+        public frmGenius()
+        {
+            InitializeComponent();
+        }
+
+        private void MostrarCor(PictureBox pic, Color corClara, Color corEscura, SoundPlayer som)
+        {
+            pic.BackColor = corClara;
+            som.Play();
+            Application.DoEvents();
+            Thread.Sleep(250);
+            pic.BackColor = corEscura;
+        }
+
+        private void ProcurarCor(string tagPic)
+        {
+            foreach (var corEncontrada in Controls.OfType<PictureBox>())
+            {
+                if (corEncontrada.Tag.ToString() == tagPic)
+                {
+                    string tag = corEncontrada.Tag.ToString();
+
+                    Color piscar = ControlPaint.Light(corEncontrada.BackColor, brilho);
+                    SoundPlayer som = tag == "R" ? audios[0] : (tag == "G" ? audios[1] : (tag == "B" ? audios[2] : audios[3]));
+                    MostrarCor(corEncontrada, piscar, corEncontrada.BackColor, som);
+
+                }
+            }
+        }
+
+        private void SortearCor()
+        {
+            atualCor = cores[rdn.Next(0, cores.Length)];
+            sequenciaCores.Add(atualCor);
+
+            foreach (var cor in sequenciaCores)
+            {
+                Application.DoEvents();
+                Thread.Sleep(50);
+                ProcurarCor(cor);
+            }
+
+            podeJogar = true;
+        }
+
+        private void Clique(object sender, EventArgs e)
+        {
+            PictureBox pb = (PictureBox)sender;
+
+            if (podeJogar)
+            {
+                podeJogar = false;
+                atualCor = pb.Tag.ToString();
+                sequenciaJogador.Add(atualCor);
+                ProcurarCor(atualCor);
+
+                if (sequenciaJogador[listaIndex] == sequenciaCores[listaIndex])
+                {
+                    pontos++;
+                    lblPontos.Text = "Pontuação: " + pontos.ToString();
+                    listaIndex++;
+                    Checar();
+                }
+                else
+                {
+                    MessageBox.Show("Sequência Incorreta! \n Fim de Jogo!");
+                }
+            }
+        }
+        private void Checar()
+        {
+            if (listaIndex == sequenciaCores.Count)
+            {
+                listaIndex = 0;
+                sequenciaJogador.Clear();
+                SortearCor();
+            }
+            else
+            {
+                podeJogar = true;
+            }
+        }
+
+        private void Opcoes(object sender, EventArgs e)
+        {
+            sequenciaCores.Clear();
+            sequenciaJogador.Clear();
+
+            ToolStripMenuItem menu = (ToolStripMenuItem)sender;
+
+            switch (menu.Text)
+            {
+            }
+        }
+
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+            sequenciaCores.Clear();
+            sequenciaJogador.Clear();
+            pontos = 0; lblPontos.Text = "Pontuação: "; sequenciaJogador.Clear();
+            podeJogar = false; listaIndex = 0; SortearCor();
+        }
+
+        private void btnSobre_Click(object sender, EventArgs e)
+        {
+            string info = "\t\tEvil Genius \nDesenvolvido por Guilherme Oliveira e Daniel Formigoni \n\t\tCopyright 2018";
+            MessageBox.Show(info);
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
